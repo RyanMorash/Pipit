c3e42f084d0136df626e4024469a6d1265baa7cb
Ryan Morash <ryan@morash.me>
2025-12-09 18:26:48 -0500

    Initial Commit

---
+++ PipitTests/PipitTests.swift
@@ -0,0 +1,16 @@
+//
+//  PipitTests.swift
+//  PipitTests
+//
+//  Created by Ryan Morash on 12/9/25.
+//
+
+import Testing
+
+struct PipitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
